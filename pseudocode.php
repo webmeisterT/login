6f38ffe5d35ff4472c32aa917a7e34fb71007816
webmeisterT <oluwatoyinakinlua@gmail.com>
2023-04-30 21:38:20 +0100

    first commit

---
+++ pseudocode.php
@@ -0,0 +1,17 @@
+<?php
+// login.php
+// step 1: using html and css, create a login form page with username and password field
+// step 2: use php to collect the username and password
+// step 3: validate and sanitize the input collected
+// step 4: get all the usernames and passwords from the database
+// step 5: check the username and password if it exists and tallies
+// step 6: if the above step 5 is correct, then start a session and store the user's username
+// step 6b: grant the user access to the next page
+// step 7: if the above step 5 is incorrect, Send response to the user and exit back to login page
+
+
+// logout.php
+// step 1: start the session
+// step 1: unset the session
+// step 2: destroy the session
+// step 3: redirect user to login page
